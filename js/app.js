ab8db55237d09c58a5c80cc30fb07b015c05a45c
ricardoAlmeidaEtic <ricardo_almeida@eticalgarve.com>
2024-02-08 21:58:05 +0000

    adição de carousel e adição de funções render especificas por cada processo

--- js/app.js
+++ js/app.js
@@ -36,6 +36,8 @@ class PecaDeArte {
        
 }
 
+let galeria = null;
+
 const getData = async() =>{
     const request = await fetch("./Data.json");
     const result = await request.json();
@@ -53,55 +55,79 @@ const loadData = (data) =>{
     return galeria
 }
 
+const showElement = (i) =>{
+    const galeriaDisplayElemento = document.querySelector("#galeriaDisplay");
+    if(galeria[i])
+        galeriaDisplayElemento.innerHTML = renderSlideShow(i, galeria[i])
+        galeriaDisplayElemento.scrollIntoView();
+}
+
+const renderSlideShow = (i,item) =>{
+    return `
+        <div class="scene">
+            <div class="cube">
+                <div class="cube__face cube__face--front">front</div>
+                <div class="cube__face cube__face--back">back</div>
+                <div class="cube__face cube__face--right">right</div>
+                <div class="cube__face cube__face--left">left</div>
+                <div class="cube__face cube__face--top">top</div>
+                <div class="cube__face cube__face--bottom">bottom</div>
+                
+                <a class="prev seta" onclick="showElement(${i-1})">&#10094;</a>
+                    ${renderElement(i,item)}
+                <a class="next seta" onclick="showElement(${i+1})">&#10095;</a>
+            </div>
+        </div>
+    `
+}
+
+const renderElement = (i,item) =>{
+    return `<div class="galeriaElemento" onclick="showElement(${i})">
+                <figure>
+                    <div class="outerBevel">
+                        <div class="flatSurface">
+                            <div class="innerBevel">
+                                <img src="${item.getImage()}">
+                            </div>
+                        </div>
+                    </div>
+                </figure>
+                <h1>${item.getType()}</h1>
+                <h1>${item.getTitle()}</h1>
+                <h1>${item.getMedium()}</h1>
+                <h1>${item.getArtist()}</h1>
+            </div>
+            `
+}
+
+const renderRowElement = (part) =>{
+    if(part === 1)
+        return `<div class="galeriaRow">`
+    
+    return `</div>`
+}
+
 window.onload = async() =>{
     let i = 0;
     const data = await getData();
-    const galeria = loadData(data)
+    galeria = loadData(data)
     const body = document.querySelector("#galeria");
     let content = ""
 
     galeria.forEach(item => {
         if (i % 3 === 0) {
-            content += `<div class="galeriaRow">`
+            content += renderRowElement(1)
         }
 
-        content += `<div class="galeriaElemento" id="${i}">`
-        //content += item.getType()
-        //content += item.getTitle()
-        //content += item.getMedium()
-        content += `<figure>
-                        <div class="outerBevel">
-                            <div class="flatSurface">
-                                <div class="innerBevel">
-                                    <img src="${item.getImage()}">`
-        //content += item.getArtist()
-        content += `                </div>
-                                </div>
-                            </div>
-                        </div>
-                    </figure>`
+        content += renderElement(i,item)
 
         if ((i + 1) % 3 === 0 || i === (galeria.length-1)) {
-            content += `</div>`
-        }
-        
-        body.innerHTML += content
-        const id = document.querySelector(i)
-
-        id.onclick = () =>{
-            document.querySelector("#galeriaDisplayElemento").innerHTML += `
-                <figure>
-                    <div class="outerBevel">
-                        <div class="flatSurface">
-                            <div class="innerBevel">
-                                <img src="${galeria[i].getImage()}">
-                            </div>
-                        </div>
-                    </div>
-                </figure>`
+            content += renderRowElement(2)
         }
 
         i++
+
     });
 
+    body.innerHTML += content
 }
